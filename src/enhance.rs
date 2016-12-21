193d65d9f27876a162fdeb3c48b255ba2eb5c99a
Mick Koch <mick@kochm.co>
2016-12-21 09:36:03 -0500

    Add some more tests to enhance module

--- src/enhance.rs
+++ src/enhance.rs
@@ -242,7 +242,68 @@ mod tests {
     }
 
     #[test]
+    fn test_frame_energy() {
+        // Test against results computed with standalone python script.
+
+        let chunks = [
+            0b001000010010,
+            0b110011001100,
+            0b111000111000,
+            0b111111111111,
+            0b10100110101,
+            0b00101111010,
+            0b01110111011,
+            0b00001000,
+        ];
+
+        let b = Bootstrap::new(&chunks);
+        let p = BaseParams::new(b.unwrap_period());
+        assert_eq!(p.harmonics, 16);
+        let (amps, _, gain_idx) = descramble(&chunks, &p);
+        let g = Gains::new(gain_idx, &amps, &p);
+        let c = Coefficients::new(&g, &amps, &p);
+        let prev = PrevFrame::default();
+        let s = Spectrals::new(&c, &p, &prev);
+
+        assert!((s.get(0) - 1.0).abs() < 0.000001);
+        assert!((s.get(1) - 0.5306769781475001).abs() < 0.000001);
+        assert!((s.get(2) - 0.3535007196676076).abs() < 0.000001);
+        assert!((s.get(3) - 0.9173875577243951).abs() < 0.000001);
+        assert!((s.get(4) - 0.13169278935782622).abs() < 0.000001);
+        assert!((s.get(5) - 4.438599873836148).abs() < 0.00001);
+        assert!((s.get(6) - 0.6796441620283217).abs() < 0.000001);
+        assert!((s.get(7) - 0.9439604687782126).abs() < 0.000001);
+        assert!((s.get(8) - 10.646341109175768).abs() < 0.000001);
+        assert!((s.get(9) - 1.3058035207362984).abs() < 0.000001);
+        assert!((s.get(10) - 1.566168983101457).abs() < 0.000001);
+        assert!((s.get(11) - 8.325591838823936).abs() < 0.00001);
+        assert!((s.get(12) - 1.5943520720633444).abs() < 0.000001);
+        assert!((s.get(13) - 1.556318849691248).abs() < 0.000001);
+        assert!((s.get(14) - 15.882295806333524).abs() < 0.00001);
+        assert!((s.get(15) - 11.386537444591385).abs() < 0.00001);
+        assert!((s.get(16) - 12.498792573794846).abs() < 0.00001);
+        assert!((s.get(17) - 12.498792573794846).abs() < 0.00001);
+        assert!((s.get(30) - 12.498792573794846).abs() < 0.00001);
+        assert!((s.get(56) - 12.498792573794846).abs() < 0.00001);
+
+        let pfe = FrameEnergy::default();
+        let fe = FrameEnergy::new(&s, &pfe, &p);
+
+        assert!((fe.energy - 752.2228257467827461).abs() < 0.0001);
+        assert!((fe.scaled - -452.3599305372189292).abs() < 0.0001);
+        assert!((fe.tracking - 71287.6111412873433437).abs() < 0.0001);
+
+        let fe = FrameEnergy::new(&s, &fe, &p);
+
+        assert!((fe.energy - 752.2228257467827461).abs() < 0.0001);
+        assert!((fe.scaled - -452.3599305372189292).abs() < 0.0001);
+        assert!((fe.tracking - 67760.8417255103122443).abs() < 0.1);
+    }
+
+    #[test]
     fn test_spectrals() {
+        // Test against results computed with standalone python script.
+
         let chunks = [
             0b001000010010,
             0b110011001100,
@@ -267,9 +328,117 @@ mod tests {
 
         assert_eq!(e.0.len(), 16);
 
-        assert!((e.get(17) - 0.0).abs() < 0.00001);
-        assert!((e.get(30) - 0.0).abs() < 0.00001);
-        assert!((e.get(56) - 0.0).abs() < 0.00001);
-        assert!((e.get(56) - 0.0).abs() < 0.00001);
+        assert!((e.get(1) - 0.46407439358059288103675044112606).abs() < 1e-6);
+        assert!((e.get(2) - 0.30913461049404472591461967567739).abs() < 1e-6);
+        assert!((e.get(3) - 0.41587411398142343221806527253648).abs() < 1e-6);
+        assert!((e.get(4) - 0.05758233878573310732251755439393).abs() < 1e-6);
+        assert!((e.get(5) - 4.29488628936570737693045884952880).abs() < 1e-6);
+        assert!((e.get(6) - 0.29717281253740035484867121340358).abs() < 1e-6);
+        assert!((e.get(7) - 0.41274456522882696507537048091763).abs() < 1e-6);
+        assert!((e.get(8) - 11.17220652652428292128661269089207).abs() < 1e-6);
+        assert!((e.get(9) - 0.61137412496551313267900695791468).abs() < 1e-6);
+        assert!((e.get(10) - 0.76976839668918539683062363110366).abs() < 1e-6);
+        assert!((e.get(11) - 8.73682419143608512968057766556740).abs() < 1e-6);
+        assert!((e.get(12) - 0.71114975707359529000228803852224).abs() < 1e-6);
+        assert!((e.get(13) - 0.68049652809620897464526478870539).abs() < 1e-6);
+        assert!((e.get(14) - 16.66679545607086865288692933972925).abs() < 1e-6);
+        assert!((e.get(15) - 10.89394258052602992847823770716786).abs() < 1e-6);
+        assert!((e.get(16) - 11.55358738738138946189337730174884).abs() < 1e-6);
+        assert_eq!(e.get(17), 0.0);
+        assert_eq!(e.get(30), 0.0);
+        assert_eq!(e.get(56), 0.0);
+    }
+
+    #[test]
+    fn test_smooth() {
+        // Test against results from standalone python script.
+
+        let chunks = [
+            0b001000010010,
+            0b110011001100,
+            0b111000111000,
+            0b111111111111,
+            0b10100110101,
+            0b00101111010,
+            0b01110111011,
+            0b00001000,
+        ];
+
+        let b = Bootstrap::new(&chunks);
+        let p = BaseParams::new(b.unwrap_period());
+        let (amps, mut voice, gain_idx) = descramble(&chunks, &p);
+        let g = Gains::new(gain_idx, &amps, &p);
+        let c = Coefficients::new(&g, &amps, &p);
+        let prev = PrevFrame::default();
+        let s = Spectrals::new(&c, &p, &prev);
+        let pfe = FrameEnergy::default();
+        let mut fe = FrameEnergy::new(&s, &pfe, &p);
+        let mut amps = EnhancedSpectrals::new(&s, &fe, &p);
+
+        let err = EnhanceErrors {
+            total: 0,
+            rate: 0.0250,
+            golay_init: 0,
+            hamming_init: 0,
+        };
+
+        // Dummy value just to get a good adaptive threshold.
+        fe.tracking = 100.0;
+
+        assert!(voice.is_voiced(1));
+        assert!(voice.is_voiced(2));
+        assert!(voice.is_voiced(3));
+        assert!(!voice.is_voiced(4));
+        assert!(!voice.is_voiced(5));
+        assert!(!voice.is_voiced(6));
+        assert!(voice.is_voiced(7));
+        assert!(voice.is_voiced(8));
+        assert!(voice.is_voiced(9));
+        assert!(!voice.is_voiced(10));
+        assert!(!voice.is_voiced(11));
+        assert!(!voice.is_voiced(12));
+        assert!(!voice.is_voiced(13));
+        assert!(!voice.is_voiced(14));
+        assert!(!voice.is_voiced(15));
+        assert!(voice.is_voiced(16));
+
+        smooth(&mut amps, &mut voice, &err, &fe, 42.0);
+
+        assert!(voice.is_voiced(1));
+        assert!(voice.is_voiced(2));
+        assert!(voice.is_voiced(3));
+        assert!(!voice.is_voiced(4));
+        assert!(!voice.is_voiced(5));
+        assert!(!voice.is_voiced(6));
+        assert!(voice.is_voiced(7));
+        assert!(voice.is_voiced(8));
+        assert!(voice.is_voiced(9));
+        assert!(!voice.is_voiced(10));
+        assert!(voice.is_voiced(11));
+        assert!(!voice.is_voiced(12));
+        assert!(!voice.is_voiced(13));
+        assert!(voice.is_voiced(14));
+        assert!(voice.is_voiced(15));
+        assert!(voice.is_voiced(16));
+
+        assert!((amps.get(1) - 0.28643362145733153312221475061961).abs() < 1e-6);
+        assert!((amps.get(2) - 0.19080248172803679351794414742471).abs() < 1e-6);
+        assert!((amps.get(3) - 0.25668369163611542971281664904382).abs() < 1e-6);
+        assert!((amps.get(4) - 0.03554067636251981299189139917871).abs() < 1e-6);
+        assert!((amps.get(5) - 2.65086772859580133143708735588007).abs() < 1e-6);
+        assert!((amps.get(6) - 0.18341948202958965885578379584331).abs() < 1e-6);
+        assert!((amps.get(7) - 0.25475208757622064270620398929168).abs() < 1e-6);
+        assert!((amps.get(8) - 6.89565211812498901622348057571799).abs() < 1e-6);
+        assert!((amps.get(9) - 0.37734920758726891998335872813186).abs() < 1e-6);
+        assert!((amps.get(10) - 0.47511250910851310358395949151600).abs() < 1e-6);
+        assert!((amps.get(11) - 5.39249790077991697501147427828982).abs() < 1e-6);
+        assert!((amps.get(12) - 0.43893221245295155341636927914806).abs() < 1e-6);
+        assert!((amps.get(13) - 0.42001258338742591957881700182043).abs() < 1e-6);
+        assert!((amps.get(14) - 10.28699416862334992117666843114421).abs() < 1e-6);
+        assert!((amps.get(15) - 6.72390346990002374383266214863397).abs() < 1e-6);
+        assert!((amps.get(16) - 7.13104606064994861469585885060951).abs() < 1e-4);
+        assert_eq!(amps.get(17), 0.0);
+        assert_eq!(amps.get(30), 0.0);
+        assert_eq!(amps.get(56), 0.0);
     }
 }
